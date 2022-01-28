aa6c09696df863f3d29d6207996b9083c8506229
Joe <josephseaton@physics.org>
2022-01-28 10:28:43 +0000

    TPP Template
    Default Third Person template as a C++ project.

---
+++ MyProject/Intermediate/Build/Win64/MyProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.30.30709
+#include "C:/Users/joesi/OneDrive/Documents/GitHub/UE4-Sample-WindowsEnv/MyProject/Intermediate/Build/Win64/MyProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
